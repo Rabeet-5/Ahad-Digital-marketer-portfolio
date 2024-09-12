36835796992f8bc12dda767c2a4ce19bf237140b
Rabeet-5 <rabeetsati@gmail.com>
2024-09-12 19:59:33 +0500

    updates

--- src/app/work/compi/P3.jsx
+++ src/app/work/compi/P3.jsx
@@ -6,9 +6,9 @@ const P3 = () => {
     <div className='lg:px-12 px-8 flex flex-col gap-10 py-12'>
       <MainHead>Some Dropshipping videos that I have created</MainHead>
       <div className='w-full justify-center flex-wrap gap-6 flex '>
-        <video className='h-[40rem] mt-6' controls autoPlay loop  src="/Videos/vid1.mp4"></video>
-        <video className='h-[40rem] mt-6' controls autoPlay loop   src="/Videos/vid2.mp4"></video>
-        <video className='h-[40rem] mt-6' controls autoPlay loop   src="/Videos/vid3.mp4"></video>
+        <video className='h-[40rem] mt-6' controls loop  src="/Videos/vid1.mp4"></video>
+        <video className='h-[40rem] mt-6' controls loop   src="/Videos/vid2.mp4"></video>
+        <video className='h-[40rem] mt-6' controls loop   src="/Videos/vid3.mp4"></video>
       </div>
     </div>
   )
